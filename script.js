8f22b3f742769280dbcb79b47f88aa69b9d05ab5
Max Heinritz <maxheinritz@gmail.com>
2020-04-18 12:52:20 -0500

    reorder

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-// https://stackoverflow.com/a/8831937
-const hashCode = str => {
-  var hash = 0;
-  for (var i = 0; i < str.length; i++) {
-    var char = str.charCodeAt(i);
-    hash = (hash << 5) - hash + char;
-    hash = hash & hash; // Convert to 32bit integer
-  }
-  return hash;
-};
-
-// Returns an array of the provided length with numeric elements
-// in the range [0, 1). The array will be the same each time this
-// function is invoked on the same day.
-//
-// eg [0.2, 0.8, 0.4, ...]
-const getSemiRandomFactorsForToday = length => {
-  const date = new Date(Date.now());
-  const d = date.getDate();
-  const m = date.getMonth() + 1; // JS months are zero-indexed.
-  const y = date.getFullYear();
-  const unit = Math.abs(hashCode(`${d}${m}${y}`)).toString();
-  const unitsNeeded = Math.ceil(length / unit.length);
-  const str = unit.repeat(unitsNeeded).substring(0, length);
-  return str.split("").map(v => v / 10);
-};
-
 // Returns a semirandom "shuffled" array containing the elements in the
 // input array, ordered in a way that's stable for today.
 //
@@ -47,4 +20,31 @@ const shuffleForToday = inputArray => {
   }
 
   return array;
+};
+
+// Returns an array of the provided length with numeric elements
+// in the range [0, 1). The array will be the same each time this
+// function is invoked on the same day.
+//
+// eg [0.2, 0.8, 0.4, ...]
+const getSemiRandomFactorsForToday = length => {
+  const date = new Date(Date.now());
+  const d = date.getDate();
+  const m = date.getMonth() + 1; // JS months are zero-indexed.
+  const y = date.getFullYear();
+  const unit = Math.abs(hashCode(`${d}${m}${y}`)).toString();
+  const unitsNeeded = Math.ceil(length / unit.length);
+  const str = unit.repeat(unitsNeeded).substring(0, length);
+  return str.split("").map(v => v / 10);
+};
+
+// https://stackoverflow.com/a/8831937
+const hashCode = str => {
+  var hash = 0;
+  for (var i = 0; i < str.length; i++) {
+    var char = str.charCodeAt(i);
+    hash = (hash << 5) - hash + char;
+    hash = hash & hash; // Convert to 32bit integer
+  }
+  return hash;
 };
